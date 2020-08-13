bb16a541d137c237e0ea8443fcfb6ed72d1a6e0d
amitable <akumar6199@gmail.com>
2020-08-13 19:33:49 +0530

    content added

--- mobile_bunny/product-list.js
+++ mobile_bunny/product-list.js
@@ -13,6 +13,19 @@ $(document).ready(function () {
     });
     $(".filter_close_icon").click(function () { 
         $(".filter_alert").hide(600);
-        
     });
+
+
+    //to hide filters and sort list once the screen width is >=992
+    //using the below method won't allow the filters and sort list
+    //pop-up again when the screen width is <=992
+    $(window).resize(function () { 
+        if($(window).width() >=992){
+            $(".filter_alert").hide();
+            $(".sort_alert").hide();
+        }
+    });
+
+
+   
 });
