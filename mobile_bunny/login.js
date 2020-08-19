9ed5293b20810571a8dfc91c8a773f528306651f
amitable <akumar6199@gmail.com>
2020-08-20 01:43:42 +0530

    email done

--- mobile_bunny/login.js
+++ mobile_bunny/login.js
@@ -1,16 +1,48 @@
 $(document).ready(function () {
-    
-    $(".inp_container .input_field").blur(function () { 
-        
-        var input=$(this).val();
 
-        if(input !==""){
+    //floating label effect
+    $(".inp_container .input_field").blur(function () {
+
+        var input = $(this).val();
+
+        if (input !== "") {
             $(this).siblings("label").addClass("active");
-        }else{
+        } else {
             $(this).siblings("label").removeClass("active");
         }
-        
+
     });
 
 
+    //color change on focus & blur of input boxes
+    $("#email_mobile").on("focus blur", function () {
+        $("#con_e_mob").toggleClass("blue-i");
+        $("#con_e_mob").removeClass("red-i");
+    });
+    $("#pass").on("focus blur", function () {
+        $("#con_pass").toggleClass("blue-i");
+        $("#con_pass").removeClass("red-i");
+    });
+
+    //form validation
+    //1)c-#con_e_mob i-#email_mobile 
+    //1)c-#con_pass i-#pass 
+
+    $("input[type=submit]").click(function () { 
+
+        var em=$("#email_mobile") .val();
+        var pw=$("#pass") .val();
+
+        if(em== ""){
+            alert("Enter your email or mobile number!");
+            $("#con_e_mob").addClass("red-i");
+            return false;
+        }
+        if(pw== ""){
+            alert("Enter your password!");
+            $("#con_pass").addClass("red-i");
+            return false;
+        }
+
+    });
 });
