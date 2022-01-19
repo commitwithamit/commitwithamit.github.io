2f5ff146705706c1158ce66c5a777a70e3831ca9
amitable <akumar6199@gmail.com>
2022-01-19 20:04:42 +0530

    hi

--- maclarg-yogita/js/navbar.js
+++ maclarg-yogita/js/navbar.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-  $(".banner-link").hover(function () {
-      // over
-      $(".banner-text").addClass("full-line");
-        setTimeout(function(){
-          $(".banner-link").addClass("pulsate-fwd");
-        }, 100);
-    }, function () {
-      // out
-      $(".banner-text").removeClass("full-line");
-      $(".banner-link").removeClass("pulsate-fwd");
-    }
-  );
-
-
     // dropdown hide/show & color bg changes
     $(".dropdown").hover(function () {
       $(".dropdown .dropdown-menu").toggleClass("display");
@@ -29,18 +15,5 @@ $(document).ready(function () {
     // hiding dropdown on click of dropdown menu
     $(".dropdown").click(function () { 
       $(".dropdown-menu").toggleClass("show");
-    });
-
-    // learn by self & book a repair slide animation on scroll position
-    $(window).scroll(function(){
-      if($(this).scrollTop()>400){
-        $(".ani-slide-left, .ani-slide-right").css("visibility", "visible");
-        $(".ani-slide-left").addClass("slide-in-left");
-        $(".ani-slide-right").addClass("slide-in-right");
-      }
     });    
 });
-
-
-
-  /* $(".product-img-con").css("transform: translateY(-20px)", "box-shadow: none").addClass("active"); */
